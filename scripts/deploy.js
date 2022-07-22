55908ece0651ba87c424955fc7d79801ccc5ff4f
gomathi <tgomathi1806@gmail.com>
2022-07-22 23:05:56 +0100

    feat(*): initial commit to call trip details smart contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,9 +3,9 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const hre = require('hardhat')
 
-async function main() {
+async function main () {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
   //
@@ -14,19 +14,31 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello world!");
+  const Greeter = await hre.ethers.getContractFactory('Greeter')
+  const greeter = await Greeter.deploy('Hello world!')
 
-  await greeter.deployed();
+  await greeter.deployed()
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log('Greeter deployed to:', greeter.address)
+
+  const TripDetails = await hre.ethers.getContractFactory('TripDetails')
+  const tripDetails = await TripDetails.deploy(
+    'schooltrip',
+    'home',
+    'school',
+    15
+  )
+
+  await tripDetails.deployed()
+
+  console.log('TripDetails deployed to:', tripDetails.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
   .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
